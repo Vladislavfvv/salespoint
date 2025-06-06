4b1598e3169373d9b3c93bfba07f73f58b2759dc
Vlad <vladfilimontsev@yandex.ru>
2025-06-06 22:32:09 +0300

    Add Eureka

--- src/main/java/com/edme/salespoint/SalesPointApplication.java
+++ src/main/java/com/edme/salespoint/SalesPointApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -12,6 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan(basePackages = "com.edme.salespoint")
 @EntityScan(basePackages = "com.edme.salespoint.models")
 @EnableJpaRepositories(basePackages = "com.edme.salespoint.repository")
+@EnableDiscoveryClient
 public class SalesPointApplication {
 
     public static void main(String[] args) {
